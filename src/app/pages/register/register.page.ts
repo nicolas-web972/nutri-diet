57967fc003af757f294e22b77eac0ae749f0f7a4
nicolas-web972 <nicolas.chalono@gmail.com>
2022-03-19 12:38:23 +0100

    create register page

---
+++ src/app/pages/register/register.page.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  credentials: FormGroup;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private loadingController: LoadingController,
+    private alertController: AlertController,
+    private fb: FormBuilder,
+    ) { }
+
+  get email() {
+    return this.credentials.get('email');
+  }
+
+  get password() {
+    return this.credentials.get('password');
+  }
+
+  ngOnInit() {
+    this.credentials = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  async register() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const userRegis = await this.auth.register(this.credentials.value);
+    await loading.dismiss();
+
+    if (userRegis) {
+      this.router.navigateByUrl('loginscreen');
+    } else {
+      await this.showAlert('Inscription réussie', 'un nouveau compte est créé !');
+    }
+  }
+
+  async showAlert(header, message) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+}
