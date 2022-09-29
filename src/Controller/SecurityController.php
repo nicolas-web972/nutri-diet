a1eae56813ac3da26f60f45fed73557f42e979e4
smectaz <90684643+smectaz@users.noreply.github.com>
2022-09-30 01:34:16 +0200

    creation login form

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('pages/security/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError()
+        ]);
+    }
+
+    #[Route('/deconnexion', name:'security.logout')]
+    public function logout()
+    {
+        //rien a faire ici le logout se fait seul
+    }
+}
